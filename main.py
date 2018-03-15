ddbd9ee6fa68d14b512139134f4279621ba0473d
vovanz <vovanzagorodsky@gmail.com>
2018-03-15 19:40:03 +0500

    shitcode

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import vk_api
+
+
+def main():
+    """ Пример получения последнего сообщения со стены """
+
+    login, password = input('login:'), input('password:')
+    vk_session = vk_api.VkApi(login, password)
+
+    try:
+        vk_session.auth()
+    except vk_api.AuthError as error_msg:
+        print(error_msg)
+        return
+
+    vk = vk_session.get_api()
+
+    for get in [vk.groups.get, vk.groups.getInvites]:
+        while True:
+            response = get(count=500)
+
+            groups = vk.groups.getById(group_ids=','.join((str(a) for a in response.get('items', []))))
+            if not groups:
+                break
+            continue_ = False
+            for group in groups:
+                if not group['is_admin']:
+                    continue_ = True
+                    vk.groups.leave(group_id=group['id'])
+            if not continue_:
+                break
+
+
+if __name__ == '__main__':
+    main()
